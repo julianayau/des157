ae06fe645474f8a4403de2828dde636c8d4e6b0a
julianayau <yaujuliana@yahoo.com>
2017-01-20 18:17:22 -0800

    Add files via upload
    uploaded exercises from book

---
+++ bookex/script_looping.js
@@ -0,0 +1,14 @@
+// js
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log('reading js');
+
+//declare empty variable first to initialize
+var n = "";
+
+// write a loop that calls seven times
+for (var i=0; i<7; i++){
+  n=n+"#";
+  console.log(n);
+}
---
+++ bookex/script_minimum.js
@@ -0,0 +1,21 @@
+// js
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log('reading js');
+
+//declare function name that takes two arguments
+function minimum(x,y){
+  // if x is less than y, return x
+  if (x<y){
+    return x;
+  }
+// else if x is greater than y, return y
+  else if (x>y){
+    return y;
+  }
+// otherwise return false
+  else {
+    return false;
+  }
+}

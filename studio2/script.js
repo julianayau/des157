22e1c50b53ef5a868d0ff735983f143fe80fcce3
julianayau <yaujuliana@yahoo.com>
2017-01-26 11:06:54 -0800

    fasd

--- studio2/script.js
+++ studio2/script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       //var song = document.getElementById('song');
 
       // user innerHTML to concatenate a message and put it on the screen
-      song.innerHTML = 'I got my '+ userBody 'eyes on you.' + "You're everything that I see I want your high" + userNoun1 + 'and emotion endlessly. I cant get over you, You left your' + userNoun3 + 'on me, I want your high' + userNoun1 + "and emotion endlessly. Cause you're a good" + userNoun2 + "and you know it. I know exactly who you could be. Just hold on we're going" + userDest + "Just hold on we're going" + userDest + "It's hard to do these things alone" + " Just hold on we're going" + userDest;
+      song.innerHTML = 'I got my '+ userBody 'eyes on you.' + "Youre everything that I see I want your high" + userNoun1 + 'and emotion endlessly. I cant get over you, You left your' + userNoun3 + 'on me, I want your high' + userNoun1 + "and emotion endlessly. Cause youre a good" + userNoun2 + "and you know it. I know exactly who you could be. Just hold on were going" + userDest + "Just hold on we're going" + userDest + "Its hard to do these things alone" + " Just hold on were going" + userDest;
 
       if (userBody == "" || userNoun1 =="" || userNoun2 == "" || userNoun3 =="" || userDest ==""){
         alert("If you're reading this, you forgot to enter a word. Thank me later.");

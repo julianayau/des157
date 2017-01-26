36f4ddfd330d332f1923cb7553f092693139e3a0
julianayau <yaujuliana@yahoo.com>
2017-01-26 12:14:12 -0800

    update javascript

--- studio2/script.js
+++ studio2/script.js
@@ -1,36 +1,36 @@
+console.log('reading js');
+
 // take user input and oupt song with users' input
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
     //capture the submit event
     document.form.onsubmit = processForm;
-    document.form.onreset= clearForm;
+    document.form.onreset = clearForm;
     //define process function
-    function processForm(){
-      //store body part singular in variable
-      var userBody = document.form.userBody.value;
-      var userNoun1 = document.form.userNoun1.value;
-      var userNoun2 = document.form.userNoun2.value;
-      var userNoun3 = document.form.userNoun3.value;
-      var userDest = document.form.userDest.value;
+    function processForm() {
+        console.log('processing form');
 
-      //var song = document.getElementById('song');
+        var userBody = document.form.userBody.value;
+        var userNoun1 = document.form.userNoun1.value;
+        var userNoun2 = document.form.userNoun2.value;
+        var userNoun3 = document.form.userNoun3.value;
+        var userDest = document.form.userDest.value;
 
-      // user innerHTML to concatenate a message and put it on the screen
-      song.innerHTML = 'I got my '+ userBody +'eyes on you.' + "Youre everything that I see I want your high" + userNoun1 + 'and emotion endlessly. I cant get over you, You left your' + userNoun3 + 'on me, I want your high' + userNoun1 + "and emotion endlessly. Cause youre a good" + userNoun2 + "and you know it. I know exactly who you could be. Just hold on were going" + userDest + "Just hold on we're going" + userDest + "Its hard to do these things alone" + " Just hold on were going" + userDest;
+        if (userBody == "" || userNoun1 == "" || userNoun2 == "" || userNoun3 == "" || userDest == "") {
+            alert("If you're reading this, you forgot to enter a word. Thank me later.");
+        } else {
+            console.log('reading the else');
 
-      if (userBody == "" || userNoun1 =="" || userNoun2 == "" || userNoun3 =="" || userDest ==""){
-        alert("If you're reading this, you forgot to enter a word. Thank me later.");
-      }
-
-      else{
-        return 0;
-      }
-      return false;
-
+            alert("you're about to see the mad libs");
+            // user innerHTML to concatenate a message and put it on the screen
+            song.innerHTML = 'I got my ' + userBody + 'eyes on you.' + "Youre everything that I see I want your high" + userNoun1 + 'and emotion endlessly. I cant get over you, You left your' + userNoun3 + 'on me, I want your high' + userNoun1 + "and emotion endlessly. Cause youre a good" + userNoun2 + "and you know it. I know exactly who you could be. Just hold on were going" + userDest + "Just hold on we're going" + userDest + "Its hard to do these things alone" + " Just hold on were going" + userDest;
+        }
+        return false;
     }
-    function clearForm(){
-      song.innerHTML = '';
+
+    function clearForm() {
+        song.innerHTML = '';
     }
 
 

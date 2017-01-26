cec642335782c1a8835d1fdfad96776f19b09443
julianayau <yaujuliana@yahoo.com>
2017-01-26 09:47:51 -0800

    updated studio 2

--- studio2/script.js
+++ studio2/script.js
@@ -15,8 +15,15 @@ function processform(){
   var song = document.getElementById('song');
 
   // user innerHTML to concatenate a message and put it on the screen
-  song.innterHTML = 'I got my '+ userBody 'eyes on you.' + 'Youre everything that I see I want your high' userNoun1 + 'and emotion endlessly. I cant get over you, You left your' userNoun3 + 'on me, I want your high' + userNoun1 + 'and emotion endlessly. Cause youre a good' + userNoun2 + 'and you know it. I know exactly who you could be. Just hold on were going' + userDest + 'Just hold on were going' + userDest + 'Its hard to do these things alone' + ' Just hold on were going' + userDest
+  song.innterHTML = 'I got my '+ userBody 'eyes on you.' + "You're everything that I see I want your high" userNoun1 + 'and emotion endlessly. I cant get over you, You left your' userNoun3 + 'on me, I want your high' + userNoun1 + "and emotion endlessly. Cause you're a good" + userNoun2 + "and you know it. I know exactly who you could be. Just hold on we're going" + userDest + "Just hold on we're going" + userDest + "It's hard to do these things alone" + " Just hold on we're going" + userDest
 
+  if (userBody == "" || userNoun1 =="" || userNoun2 == "" || userNoun3 =="" || userDest ==""){
+    alert("If you're reading this, you forgot to enter a word. Thank me later.");
+  }
+
+  else{
+    return 0;
+  }
   return false;
 
 }

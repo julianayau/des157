9bc237a61a788aaaf6160b45c361fc95a81a924c
julianayau <yaujuliana@yahoo.com>
2017-01-31 20:33:42 -0800

    ex 3

---
+++ bookex/script_cat.js
@@ -0,0 +1,21 @@
+// js
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log('reading js');
+// get images from html
+var cat = document.querySelector("#cat");
+var hat = document.querySelector("#hat");
+var angle = 0, lastTime = null;
+
+function animate(time) {
+  if (lastTime != null)
+    angle += (time - lastTime) * 0.001;
+  lastTime = time;
+  cat.style.top = (Math.sin(angle) * 20) + 40 + "px";
+  cat.style.left = (Math.cos(angle) * 200) + 250 + "px";
+  hat.style.top = (Math.sin(angle+Math.PI) * 20) + 40 + "px";
+  hat.style.left = (Math.cos(angle+Math.PI) * 200) + 250 + "px";
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);

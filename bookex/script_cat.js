79b9cdf7f1b47e6b1d9be573d56c59db064672ce
julianayau <yaujuliana@yahoo.com>
2017-02-01 11:44:36 -0800

    updated and put dom thing

--- bookex/script_cat.js
+++ bookex/script_cat.js
@@ -2,21 +2,24 @@
 
 // open the console window of any browser to see console.log() messages,
 // like print() messages in Processing
-console.log('reading js');
-// get images from html
-var cat = document.querySelector("#cat");
-var hat = document.querySelector("#hat");
-var angle = 0, lastTime = null;
+document.addEventListener("DOMContentLoaded", function(event){
+  console.log('reading js');
+  // get images from html
+  var cat = document.querySelector("#cat");
+  var hat = document.querySelector("#hat");
+  var angle = 0, lastTime = null
 
-function animate(time) {
-  if (lastTime != null)
-    angle += (time - lastTime) * 0.001;
-  lastTime = time;
-  cat.style.top = (Math.sin(angle) * 20) + 40 + "px";
-  cat.style.left = (Math.cos(angle) * 200) + 250 + "px";
-  // plus pi so that hat is opposite of cat
-  hat.style.top = (Math.sin(angle+Math.PI) * 20) + 40 + "px";
-  hat.style.left = (Math.cos(angle+Math.PI) * 200) + 250 + "px";
+  function animate(time) {
+    if (lastTime != null)
+      angle += (time - lastTime) * 0.001;
+    lastTime = time;
+    cat.style.top = (Math.sin(angle) * 20) + 70 + "px";
+    cat.style.left = (Math.cos(angle) * 200) + 300 + "px";
+    // plus pi so that hat is opposite of cat
+    hat.style.top = (Math.sin(angle+Math.PI) * 20) + 70 + "px";
+    hat.style.left = (Math.cos(angle+Math.PI) * 200) + 300 + "px";
+    requestAnimationFrame(animate);
+  }
   requestAnimationFrame(animate);
-}
-requestAnimationFrame(animate);
+
+});

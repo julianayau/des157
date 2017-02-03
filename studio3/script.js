01a68e838a8d61f288426a827a6a5b59c311f90d
julianayau <yaujuliana@yahoo.com>
2017-02-02 21:13:25 -0800

    poop

--- studio3/script.js
+++ studio3/script.js
@@ -2,51 +2,114 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     var princessbox = document.getElementById("princessbox");
     var pbbox = document.getElementById("pbbox");
-    var luckycharmsbox = document.getElementById("luckycharmsbox");
     var gsbox = document.getElementById("gsbox");
+    var kravebox = document.getElementById("kravebox");
+    var ffbox = document.getElementById("ffbox");
 
-    var princesscontent = document.getElementById("princesscontent");
+    var hideprincess = document.getElementById("hideprincess");
+    var hidepb = document.getElementById("hidepb");
+    var hidegs = document.getElementById("hidegs");
+    var hidekrave = document.getElementById("hidekrave");
+    var hideff = document.getElementById("hideff");
+
+    var close1 = document.getElementById('close1');
+    var close2 = document.getElementById('close2');
+    var close3 = document.getElementById('close3');
+    var close4 = document.getElementById('close4');
+    var close5 = document.getElementById('close5');
+
 
     princessbox.addEventListener('mouseover', function() {
         princessbox.style.cursor = 'pointer';
     })
 
-     princessbox.addEventListener('mouseout', function() {
-       // back to set image
-       princessbox.src = "images/princessbox.jpg";
-     })
-
-    princessbox.addEventListener('click', function(){
-      princessbox.src = "images/open.jpg";
-      princesscontent.style.display = "block";
+    princessbox.addEventListener('mouseout', function() {
+        princessbox.src = "images/princess.png";
     })
 
+    princessbox.addEventListener('click', function() {
+        princessbox.src = "images/princessopen.png";
+        hideprincess.style.display = 'block';
 
-    // pbbox.addEventListener('mouseover', function() {
-    //     // gif of box moving side to side
-    //     pbbox.src = "images/background.jpg";
-    // })
-    // pbbox.addEventListener('mouseout', function() {
-    //     // back to set image
-    //     pbbox.src = "images/pbbox.jpg";
-    // })
-    // luckycharmsbox.addEventListener('mouseover', function() {
-    //     // gif of box moving side to side
-    //     luckycharmsbox.src = "images/background.jpg";
-    // })
-    // luckycharmsbox.addEventListener('mouseout', function() {
-    //     //back to set image
-    //     luckycharmsbox.src = "#";
-    // })
-    // gsbox.addEventListener('mouseover', function() {
-    //     // gif of box moving side to side
-    //     gsbox.src = "images/background.jpg";
-    // })
-    // gsbox.addEventListener('mouseout', function() {
-    //     // back to set image
-    //     gsbox.src = "#";
-    // })
+    })
 
+    close1.addEventListener('click', function() {
+        hideprincess.style.display = "none";
+    })
 
+    // capn' crunch box
+    pbbox.addEventListener('mouseover', function() {
+        pbbox.style.cursor = 'pointer';
+    })
+
+    pbbox.addEventListener('mouseout', function() {
+        // back to set image
+        pbbox.src = "images/pb.png";
+    })
+
+    pbbox.addEventListener('click', function() {
+        pbbox.src = "images/pbopen.png";
+        hidepb.style.display = 'block';
+    })
+
+    close2.addEventListener('click', function() {
+        hidepb.style.display = "none";
+    })
+    // gs box
+    gsbox.addEventListener('mouseover', function() {
+        gsbox.style.cursor = 'pointer';
+    })
+
+    gsbox.addEventListener('mouseout', function() {
+        // back to set image
+        gsbox.src = "images/gs.png";
+    })
+
+    gsbox.addEventListener('click', function() {
+        gsbox.src = "images/gsopen.png";
+        hidegs.style.display = 'block';
+    })
+
+    close3.addEventListener('click', function() {
+        hidegs.style.display = "none";
+    })
+
+    // krave box
+    kravebox.addEventListener('mouseover', function() {
+        kravebox.style.cursor = 'pointer';
+    })
+
+    kravebox.addEventListener('mouseout', function() {
+        // back to set image
+        kravebox.src = "images/krave.png";
+    })
+
+    kravebox.addEventListener('click', function() {
+        kravebox.src = "images/kraveopen.png";
+        hidekrave.style.display = 'block';
+    })
+
+    close4.addEventListener('click', function() {
+        hidekrave.style.display = "none";
+    })
+
+    // frosted flakes box
+    ffbox.addEventListener('mouseover', function() {
+        ffbox.style.cursor = 'pointer';
+    })
+
+    ffbox.addEventListener('mouseout', function() {
+        // back to set image
+        ffbox.src = "images/ff.png";
+    })
+
+    ffbox.addEventListener('click', function() {
+        ffbox.src = "images/ffopen.png";
+        hideff.style.display = 'block';
+    })
+
+    close5.addEventListener('click', function() {
+        hideff.style.display = "none";
+    })
 
 });

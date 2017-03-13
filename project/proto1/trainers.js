af6eea0a69875143971ac045c7c704f5694a75b2
julianayau <yaujuliana@yahoo.com>
2017-03-12 22:23:17 -0700

    yay

--- project/proto1/trainers.js
+++ project/proto1/trainers.js
@@ -13,38 +13,48 @@ var trainers = [
   {"name": "Deboarah Sanders", "email": "deborah_sanders@yahoo.com", "goal": "lose body fat", "diet_style": "IIFYM", "coaching_style": "in-house"},
   {"name": "Juan Cox", "email": "juan_cox@yahoo.com", "goal": "Bodybuilding", "diet_style": "IIFYM", "coaching_style": "in-house"},
   {"name": "Ann Adams", "email": "ann_adams@yahoo.com", "goal": "Gain Muscle", "diet_style": "IIFYM", "coaching_style": "in-house"},
-    {"name": "Gregory Thomas", "email": "gregory_thomas@yahoo.com", "goal": "Powerlifting", "diet_style": "IIFYM", "coaching_style": "in-house"},
-    {"name": "Judith Mitchel", "email": "judith_mitchell@yahoo.com", "goal": "reverse diet", "diet_style": "IIFYM", "coaching_style": "online"}
+  {"name": "Gregory Thomas", "email": "gregory_thomas@yahoo.com", "goal": "Powerlifting", "diet_style": "IIFYM", "coaching_style": "in-house"},
+  {"name": "Judith Mitchel", "email": "judith_mitchell@yahoo.com", "goal": "reverse diet", "diet_style": "IIFYM", "coaching_style": "online"}
 
 ];
 
 
-/***** START PROCESSING FORM *****/
+var client_start = document.getElementById('client_start');
+var next = document.getElementById('next');
+var find_coach = document.getElementById('find_coach');
+var articles = document.getElementsByTagName('article');
+
+client_start.addEventListener('click', function(){
+  for( var i=0; i<articles.length; i++){
+    articles[i].className = "back";
+  }
+  articles[1].className = "front";
+})
+
+next.addEventListener('click', function(){
+  processf1();
+  for( var i=0; i<articles.length; i++){
+    articles[i].className = "back";
+  }
+
+  articles[2].className = "front";
+})
 
 
+find_coach.addEventListener('click', function(){
+  processf2();
+  for( var i=0; i<articles.length; i++){
+    articles[i].className = "back";
 
-document.getElementsByClassName("next").onclick = processf1;
+  }
 
-function processf1(){
-console.log('poo');
+  articles[3].className = "front";
 
-          var client_name = document.form.client_name.value;
+})
 
-          if (client_name == "" ) {
-              alert("Please enter your name");
-          } else {
 
-              // user innerHTML to concatenate a message and put it on the screen
-              greetings.innerHTML = 'Hello ' + client_name + " 'let's answer some questions about yourself!" + ".";
-
-          }
-
-}
-
-document.getElementsByClassName("find_coach").onclick = processf2;
 function processf2(){
 
-  var client_goal = document.getElementById('client_goal');
   var client_goal_value = client_goal.options[client_goal.selectedIndex].text;
 
   var client_diet = document.getElementById("client_diet");
@@ -55,31 +65,54 @@ function processf2(){
 
 
   find_trainer(client_goal_value, client_diet_value, client_coach_value);
+
 }
 
 
 function find_trainer(client_goal_value, client_diet_value, client_coach_value){
 
   var found = "";
-
+;
   for (var i=0; i<trainers.length; i++){
 
     if(trainers[i].goal == client_goal_value){
-      console.log(trainers[i].goal);
+      console.log(trainers[0].goal );
       if(trainers[i].diet_style == client_diet_value){
 
         if(trainers[i].coaching_style == client_coach_value){
 
           found = '<div class="results">' + trainers[i].name + '</p><p>' + trainers[i].email + '</p><p>' + trainers[i].goal + '</p><p>' + trainers[i].diet_style + '</p><p>' + trainers[i].coaching_style + '</p></div>';
+
         }
 
       }
     }
   }
+  document.getElementById("results").innerHTML = found;
 
-  document.getElementById("description").innerHTML = found;
+
 }
 
+
+/***** START PROCESSING FORM *****/
+
+
+
+
+function processf1(){
+  var greetings = document.getElementById("greetings");
+
+  var client_name = document.getElementById('client_name').value;
+          if (client_name == "" ) {
+              alert("Please enter your name");
+          } else {
+
+              // user innerHTML to concatenate a message and put it on the screen
+              greetings.innerHTML = 'Hello ' + client_name + " 'let's answer some questions about yourself!" + ".";
+
+            }
+
+}
 
 
 
